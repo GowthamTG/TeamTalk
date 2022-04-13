ab9adf0e88f8688ccde47e423185cf0c76554b39
Gowtham T G <gowthamgopinath1@gmail.com>
2022-04-13 20:44:42 +0530

    Files Updated

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -19,7 +19,6 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatInputModule } from '@angular/material/input';
 
 import { AppComponent } from './app.component';
 import { ErrorPageComponent } from './error-page/error-page.component';
